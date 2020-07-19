537703478215e3294a76dcbae40bba26a1bc97d3
Ngo Van Thien <thienyl6716@gmail.com>
2020-07-19 16:38:10 +0700

    Updates

---
+++ precache-manifest.5ec4a533130967f8663ab3623c6f59cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b82b73bc27145d2eb1905db5656be07",
+    "url": "/bmg/index.html"
+  },
+  {
+    "revision": "5718b922ff70d1877e8b",
+    "url": "/bmg/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4107f74c6b1f7acf3036",
+    "url": "/bmg/static/js/2.294c408e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bmg/static/js/2.294c408e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5718b922ff70d1877e8b",
+    "url": "/bmg/static/js/main.42944b10.chunk.js"
+  },
+  {
+    "revision": "3fe0a024e703ce278643",
+    "url": "/bmg/static/js/runtime-main.ca5b39af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bmg/static/media/logo.5d5d9eef.svg"
+  }
+]);
