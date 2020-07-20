3b23258b57b8414114f12df4dcccaab0568f1d13
Ngo Van Thien <thienyl6716@gmail.com>
2020-07-20 23:02:20 +0700

    Add fields

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FacebookLogin from 'react-facebook-login';
 
 function App() {
+
+  function onCallback(response) {
+    console.log(response);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="test">
+      <FacebookLogin
+        appId="720608581844868"
+        cookie={true}
+        fields="name,email,picture"
+        callback={onCallback}/>
     </div>
   );
 }
